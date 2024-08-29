3e4b08cf9a25eb9d897871349931e67aa127fe4a
Raghed <raqed68@gmail.com>
2024-08-29 21:23:37 +0300

    this is second update

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -15,6 +15,7 @@ contract VotingSystem is ERC20, Ownable {
         string description;
         uint256 yesVotes;
         uint256 noVotes;
+        bool isApproved; 
         mapping (address => bool) hasVoted;
     }
 
@@ -23,6 +24,7 @@ contract VotingSystem is ERC20, Ownable {
     function submitProposal(string memory description) public {
         proposalCount++;
 
+        // function te create a new proposal 
         Proposal storage newProposal = proposals[proposalCount];
         newProposal.id = proposalCount;
         newProposal.proposer = msg.sender;
@@ -36,8 +38,31 @@ contract VotingSystem is ERC20, Ownable {
         _mint(to, amount);
     }
 
-    // Function to allow voting (requires token balance)
+    // Function to allow voting (need token check) 
+    function voting(uint256 proposalId, bool isYes) public {
+        Proposal storage proposal = proposals[proposalId];     
+        require(balanceOf(msg.sender) > 0, "You must hold tokens to vote");
+        require(proposal.hasVoted[msg.sender] = false , "Already Voted");
+        proposal.hasVoted[msg.sender] = true;
+
+        if (isYes) {
+            proposals[proposalId].yesVotes++;
+        } else {
+            proposals[proposalId].noVotes++;
+        }
+    }
+
     // Function to get proposal details
+    function getProposal(uint256 proposalId) public view returns(uint256, address, string memory, uint256, uint256) {
+        Proposal storage proposal = proposals[proposalId];
+        return (
+            proposal.id,
+            proposal.proposer,
+            proposal.description,
+            proposal.yesVotes,
+            proposal.noVotes
+        );
+    }
 }
 
 
