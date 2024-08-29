f329c1f9167c9dbf6898c01660e1c43fa8ab758b
Raghed <raqed68@gmail.com>
2024-08-29 20:40:36 +0300

    Initial commit

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,29 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzepplin/contracts/token/ERC20/ERC20.sol";
-import "@openzepplin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract VotingSystem is ERC20, Ownable {
+    uint256 public proposalCount;
 
-contract VotingSystem {
-    uint256 public proposalCount; 
-
-    constrctor() ERC20("VoteToken", "RMA") Ownable(msg.sender) {}
+    constructor() ERC20("VoteToken", "RMA") Ownable(msg.sender) {}
 
     struct Proposal {
         uint256 id;
-        address propser;
+        address proposer;
         string description;
         uint256 yesVotes;
         uint256 noVotes;
         mapping (address => bool) hasVoted;
     }
 
-    mapping(uint256 => Proposal) public Proposals; 
+    mapping(uint256 => Proposal) public proposals;
 
+    function submitProposal(string memory description) public {
+        proposalCount++;
+
+        Proposal storage newProposal = proposals[proposalCount];
+        newProposal.id = proposalCount;
+        newProposal.proposer = msg.sender;
+        newProposal.description = description;
+        newProposal.yesVotes = 0;
+        newProposal.noVotes = 0;
+    }
+
+    // mint
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Add more functions as needed
+    // - Function to allow voting (requires token balance)
+    // - Function to get proposal details
 }
 
-// make sure anyone do have token only making voting, 
-// token is the identity of the token, 
-// also check if the this person has made token or not 
 
